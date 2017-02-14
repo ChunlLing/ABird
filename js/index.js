9f3427f8a97e235eed096f14540dab83db4187a7
ChunLing <xcl8202@qq.com>
2017-02-14 16:57:59 +0800

    完善表单验证

--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
 ;$(function () {
 	// 注册对话框表单验证
-	$('#reg-submit').click(function () {
-		$('#reg').submit();
-	});
 	$('#reg').validate({
-		debug : true,
+		errorElement : 'span',
+		errorClass : 'help-block',
 		rules : {
 			user : {
 				required : true,
@@ -18,6 +16,10 @@
 				required : true,
 				minlength : 6,
 			},
+			notpass : {
+				required : true,
+				equalTo : '#pass',
+			},
 			email : {
 				required : true,
 				email : true,
@@ -25,19 +27,59 @@
 		},
 		messages : {
 			user : {
-				required : '账号不得为空！',
-				minlength : '账号不得小于{0}位！',
-				remote : '账号被占用！',
+				required : '昵称不得为空！',
+				minlength : '昵称不得小于{0}位！',
+				remote : '昵称被占用！',
 			},
 			pass : {
 				required : '密码不得为空！',
 				minlength : '密码不得小于{0}位！',
+			},
+			notpass : {
+				required : '请再输入密码',
+				equalTo : '两次密码不一致',
 			},
 			email : {
 				required : '邮箱不得为空！',
 				email : '请输入正确的邮箱格式！',
 			},
 		},
+		errorPlacement : function(error, element) {
+			element.next().remove();
+			element.after('<span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>');
+			element.closest('.form-group').append(error);
+		},
+		highlight : function(element) {
+			$(element).closest('.form-group').addClass('has-error has-feedback');
+		},
+		success : function(label) {
+			var el=label.closest('.form-group').find("input");
+			el.next().remove();
+			el.after('<span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>');
+			label.closest('.form-group').removeClass('has-error').addClass("has-feedback has-success");
+			label.remove();
+		},
+		submitHandler: function(form) { 
+			$(form).ajaxSubmit({
+				url : 'add_user.php',
+				type : 'POST',
+				beforeSubmit : function (formData, jqForm, options) {
+					$('#loading-dialog').modal('show');
+					$('#reg').find('button').each(function (index) {
+						$(this).addClass('disabled');
+					});
+				},
+				success : function (responseText, statusText) {
+					if (responseText) {
+						$('#loading-dialog').modal('hidden');
+						$('#reg').find('button').each(function (index) {
+							$(this).remoteClass('disabled');
+						});
+						$('#success-dialog').modal('show');
+					}
+				},
+			});
+		}
 	});
 			/*
 	$('#reg').validate({
@@ -77,17 +119,6 @@
 		unhightlight : function (element, errorClass) {
 			$(element).removeClass('text-danger');
 		},
-
-		// errorLabelContainer : 'ol.reg-error',
-		// wrapper : 'li',
 	});
 			*/
-	$('#question').validate({
-		rules : {
-			login_pass : {
-				required : true,
-				minlength : 2,
-			}, 
-		}
-	});
 });
