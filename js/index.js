6f1be0ff02018f438cdacaa7fd33713ec27dcd3f
ChunLing <xcl8202@qq.com>
2017-04-09 21:55:14 +0800

    本地注册

--- js/index.js
+++ js/index.js
@@ -82,47 +82,47 @@
 	$('#reg').validate({
 		errorElement : 'span',
 		errorClass : 'help-block',
-		rules : {
-			user : {
-				required : true,
-				minlength : 2,
-				remote : {
-					url : 'is-user.php',
-					type : 'POST',
-				},
-			},
-			pass : {
-				required : true,
-				minlength : 6,
-			},
-			notpass : {
-				required : true,
-				equalTo : '#pass',
-			},
-			email : {
-				required : true,
-				email : true,
-			},
-		},
-		messages : {
-			user : {
-				required : '昵称不得为空！',
-				minlength : '昵称不得小于{0}位！',
-				remote : '昵称被占用！',
-			},
-			pass : {
-				required : '密码不得为空！',
-				minlength : '密码不得小于{0}位！',
-			},
-			notpass : {
-				required : '请再输入密码',
-				equalTo : '两次密码不一致',
-			},
-			email : {
-				required : '邮箱不得为空！',
-				email : '请输入正确的邮箱格式！',
-			},
-		},
+		// rules : {
+		// 	user : {
+		// 		required : true,
+		// 		minlength : 2,
+		// 		// remote : {
+		// 		// 	url : 'is-user.php',
+		// 		// 	type : 'POST',
+		// 		// },
+		// 	},
+		// 	pass : {
+		// 		required : true,
+		// 		minlength : 6,
+		// 	},
+		// 	notpass : {
+		// 		required : true,
+		// 		equalTo : '#pass',
+		// 	},
+		// 	email : {
+		// 		required : true,
+		// 		email : true,
+		// 	},
+		// },
+		// messages : {
+		// 	user : {
+		// 		required : '昵称不得为空！',
+		// 		minlength : '昵称不得小于{0}位！',
+		// 		// remote : '昵称被占用！',
+		// 	},
+		// 	pass : {
+		// 		required : '密码不得为空！',
+		// 		minlength : '密码不得小于{0}位！',
+		// 	},
+		// 	notpass : {
+		// 		required : '请再输入密码',
+		// 		equalTo : '两次密码不一致',
+		// 	},
+		// 	email : {
+		// 		required : '邮箱不得为空！',
+		// 		email : '请输入正确的邮箱格式！',
+		// 	},
+		// },
 		errorPlacement : function(error, element) {
 			element.next().remove();
 			element.after('<span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>');
@@ -139,37 +139,59 @@
 			label.remove();
 		},
 		submitHandler: function(form) { 
-			$(form).ajaxSubmit({
-				url : 'add_user.php',
-				type : 'POST',
-				beforeSubmit : function (formData, jqForm, options) {
-					$('#loading-dialog').modal('show');
-					$('#reg').find('button').each(function (index) {
-						$(this).addClass('disabled');
-					});
-				},
-				success : function (responseText, statusText) {
-					if (responseText) {
-						$('#loading-dialog').modal('hide');
-						$('#reg').find('button').each(function (index) {
-							$(this).removeClass('disabled');
-						});
-						$('#success-dialog').modal('show');
-						$.cookie('user', $('#user').val());
-						setTimeout(function () {
-							$('#success-dialog').modal('hide');
-							$('#modal-reg').modal('hide');
-							$('#reg').resetForm();
-							$('#reg').find('input').each(function (index) {
-								$(this).removeClass().addClass('form-control');
-							});
-							$('#reg-a, #login-a').hide();
-							$('#member-dropdown, #logout').show();
-							$('#member').html($.cookie('user'));
-						}, 1000);
-					}
-				},
+			$('#loading-dialog').modal('show');
+			$('#reg').find('button').each(function (index) {
+				$(this).addClass('disabled');
 			});
+			alert('此处仅为实现表单验证，并不对表单数据作后台提交处理，关闭此弹窗后进入模拟登陆状态');
+			setTimeout(function () {
+				$('#loading-dialog').modal('hide');
+				$('#reg').find('button').each(function (index) {
+					$(this).removeClass('disabled');
+				});
+				$('#success-dialog').modal('show');
+			}, 2000)
+			setTimeout(function () {
+				$('#success-dialog').modal('hide');
+				$('#modal-reg').modal('hide');
+				$('#reg').resetForm();
+				$('#reg').find('input').each(function (index) {
+					$(this).removeClass().addClass('form-control');
+				});
+				$('#reg-a, #login-a').hide();
+				$('#member-dropdown, #logout').show();
+			}, 4000);
+			// $(form).ajaxSubmit({
+			// 	url : 'add_user.php',
+			// 	type : 'POST',
+			// 	beforeSubmit : function (formData, jqForm, options) {
+			// 		$('#loading-dialog').modal('show');
+			// 		$('#reg').find('button').each(function (index) {
+			// 			$(this).addClass('disabled');
+			// 		});
+			// 	},
+			// 	success : function (responseText, statusText) {
+			// 		if (responseText) {
+			// 			$('#loading-dialog').modal('hide');
+			// 			$('#reg').find('button').each(function (index) {
+			// 				$(this).removeClass('disabled');
+			// 			});
+			// 			$('#success-dialog').modal('show');
+			// 			$.cookie('user', $('#user').val());
+			// 			setTimeout(function () {
+			// 				$('#success-dialog').modal('hide');
+			// 				$('#modal-reg').modal('hide');
+			// 				$('#reg').resetForm();
+			// 				$('#reg').find('input').each(function (index) {
+			// 					$(this).removeClass().addClass('form-control');
+			// 				});
+			// 				$('#reg-a, #login-a').hide();
+			// 				$('#member-dropdown, #logout').show();
+			// 				$('#member').html($.cookie('user'));
+			// 			}, 1000);
+			// 		}
+			// 	},
+			// });
 		}
 	});
 
