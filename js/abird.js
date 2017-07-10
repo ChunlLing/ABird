65ff514690df61817c5df407dc806b1250d09693
ChunLing <xcl8202@qq.com>
2017-07-10 20:50:24 +0800

    修复编辑器在标签页加载前完成点击事件绑定导致无法加载编辑框

--- js/abird.js
+++ js/abird.js
@@ -1,21 +1,23 @@
 $(function () {
 	// $('#login-btn').trigger('click');
 	// $('#reg-btn').trigger('click');
-	$('.addNote').click(function () {
-		// 获取编辑器
-		var ue = UE.getEditor('editor-container');
-		ue.ready(function () {
-			var html = ue.getContent();
-		});
-	});
 	$('#nav-list a[data-toggle="tab"]').on('show.bs.tab', function (e) {
 		var url = $(this).attr('href');
 		var target = $(this).data('target');
 		var tab = $(this);
 		$(target).load(url, function (result) {
 			tab.tab('show');
+			$('.addNote').click(function () {
+				// 获取编辑器
+				var ue = UE.getEditor('editor-container');
+				ue.ready(function () {
+					var html = ue.getContent();
+				});
+			});
 		});
+	
 	});
 	$('#nav-home a').tab('show');
 	// $('#login-modal').modal('show');
+	
 });
