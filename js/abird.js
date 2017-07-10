830ca619846bf2c489db0993e3f4fd65cda5e0a7
ChunLing <xcl8202@qq.com>
2017-07-10 19:14:49 +0800

    jQuery动态加载标签页

--- js/abird.js
+++ js/abird.js
@@ -1,6 +1,6 @@
 $(function () {
 	// $('#login-btn').trigger('click');
-	$('#reg-btn').trigger('click');
+	// $('#reg-btn').trigger('click');
 	$('.addNote').click(function () {
 		// 获取编辑器
 		var ue = UE.getEditor('editor-container');
@@ -8,5 +8,14 @@ $(function () {
 			var html = ue.getContent();
 		});
 	});
+	$('#nav-list a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+		var url = $(this).attr('href');
+		var target = $(this).data('target');
+		var tab = $(this);
+		$(target).load(url, function (result) {
+			tab.tab('show');
+		});
+	});
+	$('#nav-home a').tab('show');
 	// $('#login-modal').modal('show');
 });
