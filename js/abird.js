02174a2c1b1e310866a27b6801db7f7853bd0e3d
ChunLing <xcl8202@qq.com>
2017-07-15 22:15:06 +0800

    编辑按钮绑定更改为事件委托

--- js/abird.js
+++ js/abird.js
@@ -1,27 +1,34 @@
 $(function () {
-	// $('#login-btn').trigger('click');
-	// $('#reg-btn').trigger('click');
+
 	$('#nav-list a[data-toggle="tab"]').on('show.bs.tab', function (e) {
 		var url = $(this).attr('href');
 		var target = $(this).data('target');
 		var tab = $(this);
 		$(target).load(url, function (result) {
 			tab.tab('show');
-			$('.addNote').click(function () {
-				// 获取编辑器
-				var ue = UE.getEditor('editor-container');
-				ue.ready(function () {
-					var html = ue.getContent();
-				});
-				console.log('12');
-			});
 			$('#to-myNote').click(function () {
 				$('#nav-myNote a').tab('show');
 			});
+
 		});
 	});
 	$('#nav-home a').tab('show');
+
 	$('body').on('hidden.bs.modal', '#remote-modal', function () {
 		$(this).removeData('bs.modal');
 	});
+
+	$('body').on('click', '.addNote', function () {
+		// 获取编辑器
+		var ue = UE.getEditor('editor-container');
+		ue.ready(function () {
+			var html = ue.getContent();
+		});
+	});
+
+	$('body').on('click', '#submit', function () {
+		alert();
+		return false;
+	});
+
 });
