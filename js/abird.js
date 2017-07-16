43a8b2d6c2a075dc806e254c7558561106d1210b
ChunLing <xcl8202@qq.com>
2017-07-16 23:40:41 +0800

    注册表单验证

--- js/abird.js
+++ js/abird.js
@@ -19,18 +19,46 @@ $(function () {
 		});
 	}).on('click', '#to-myNote', function () {
 		$('#nav-myNote a').tab('show');
-	}).on('focus', '#useremail', function () {
+	}).on('focus', '#reg-username', function () {
+		if ($(this).next('span')) {
+			$(this).next('span').remove();
+		}
+		$(this).parents('.form-group').attr('class', 'form-group has-feedback');
+	}).on('blur', '#reg-username', function () {
+		var pattern = /[\w\u4e00-\u9fa5]/;
+		if (pattern.test($(this).val())) {
+			$(this).after('<span class="glyphicon glyphicon-ok form-control-feedback"></span>');
+			$(this).parents('.form-group').addClass('has-success');
+		} else {
+			$(this).after('<span class="glyphicon glyphicon-remove form-control-feedback"></span>');
+			$(this).parents('.form-group').addClass('has-error');
+		}
+	}).on('focus', '#reg-useremail', function () {
 		var host = ['qq.com', 'gmail.com', 'sina.com', '126.com', '163.com'];
 		for (var i = 0; i < host.length; i++) {
 			$('.email-list').append('<li class="email-list-item"><span class="userinput"></span>@' + host[i] + '</li>');
 		}
+		if ($(this).next('span')) {
+			$(this).next('span').remove();
+		}
+		$(this).parents('.form-group').attr('class', 'form-group has-feedback');
 		$('.email-list').removeClass('hide');
-	}).on('keyup', '#useremail', function (e) {
+	}).on('blur', '#reg-useremail', function () {
+		$('.email-list').addClass('hide');
+		var pattern = /^[\w]+@[\w]{2,4}\.[\w]{2,3}$/;
+		if (pattern.test($(this).val())) {
+			$(this).after('<span class="glyphicon glyphicon-ok form-control-feedback"></span>');
+			$(this).parents('.form-group').addClass('has-success');
+		} else {
+			$(this).after('<span class="glyphicon glyphicon-remove form-control-feedback"></span>');
+			$(this).parents('.form-group').addClass('has-error');
+		}
+	}).on('keyup', '#reg-useremail', function (e) {
 		e.preventDefault();
 		if (e.keyCode != 13 || e.keyCode != 38 || e.keyCode != 40) {
 			$('.userinput').text($(this).val());
 		}
-	}).on('keydown', '#useremail', function (e) {
+	}).on('keydown', '#reg-useremail', function (e) {
 		switch (e.keyCode) {
 			case 13:
 				e.preventDefault();
@@ -54,11 +82,36 @@ $(function () {
 				}
 				break;
 		}
-	}).on('blur', '#useremail', function () {
-		$('.email-list').addClass('hide');
 	}).on('mousedown', '.email-list-item', function () {
-		$('#useremail').val($(this).text());
+		$('#reg-useremail').val($(this).text());
 		$('.email-list').addClass('hide');
+	}).on('focus', '#reg-userPSW', function () {
+		if ($(this).next('span')) {
+			$(this).next('span').remove();
+		}
+		$(this).parents('.form-group').attr('class', 'form-group has-feedback');
+	}).on('blur', '#reg-userPSW', function () {
+		var pattern = /^[\w]{6,20}$/;
+		if (pattern.test($(this).val())) {
+			$(this).after('<span class="glyphicon glyphicon-ok form-control-feedback"></span>');
+			$(this).parents('.form-group').addClass('has-success');
+		} else {
+			$(this).after('<span class="glyphicon glyphicon-remove form-control-feedback"></span>');
+			$(this).parents('.form-group').addClass('has-error');
+		}
+	}).on('focus', '#reg-userPSWAgain', function () {
+		if ($(this).next('span')) {
+			$(this).next('span').remove();
+		}
+		$(this).parents('.form-group').attr('class', 'form-group has-feedback');
+	}).on('blur', '#reg-userPSWAgain', function () {
+		if ($(this).val() == $('#reg-userPSW').val()) {
+			$(this).after('<span class="glyphicon glyphicon-ok form-control-feedback"></span>');
+			$(this).parents('.form-group').addClass('has-success');
+		} else {
+			$(this).after('<span class="glyphicon glyphicon-remove form-control-feedback"></span>');
+			$(this).parents('.form-group').addClass('has-error');
+		}
 	}).on('click', '#reg-submit', function () {
 		var option = {
 			type: 'POST',
