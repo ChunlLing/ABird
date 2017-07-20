382e366fa02af86ad4aa732ea39ccc80d146f8b0
ChunLing <xcl8202@qq.com>
2017-07-21 01:47:30 +0800

    服务器端读取数据库用户笔记

---
+++ data/show_note.php
@@ -0,0 +1,16 @@
+<?php 
+	require 'config.php';
+	$start = $_GET['start'];
+	if (!isset($start)) {
+		$start = 0;
+	}
+	$count = $_GET['count'];
+	$user = $_GET['user'];
+	$sql = "SELECT name, title, label, content, date FROM notetable WHERE name = '{$user}' ORDER BY date DESC LIMIT $start, $count";
+	$result = mysqli_query($conn, $sql);
+	$output = [];
+	while (($row = mysqli_fetch_assoc($result)) !== NULL) {
+		$output[] = $row;
+	}
+	echo json_encode($output);
+?>
