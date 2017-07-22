1f2855dc6a4a4b646f2abf73358143779ab89a45
ChunLing <xcl8202@qq.com>
2017-07-22 15:53:43 +0800

    补充注册密码填写符合格式后与确认密码（若已填写）相比较

--- js/abird.js
+++ js/abird.js
@@ -119,6 +119,10 @@ $(function () {
 		validityFocus($(this));
 	}).on('blur', '#reg-userPSW', function () {
 		validityBlur($(this), /^[\w]{6,20}$/, '密码长度必须在6-20位之间！');
+		if ($(this).val() && $('#reg-userPSWAgain').val()) {
+			validityFocus($('#reg-userPSWAgain'));
+			validityBlur($('#reg-userPSWAgain'), "$('#reg-userPSWAgain').val() == $('#reg-userPSW').val()", '两次输入密码不一致！');
+		}
 	}).on('focus', '#reg-userPSWAgain', function () {
 		validityFocus($(this));
 	}).on('blur', '#reg-userPSWAgain', function () {
