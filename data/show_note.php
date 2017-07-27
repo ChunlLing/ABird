4078a6c195869db73d33b2c7e5e87349915a6582
ChunLing <xcl8202@qq.com>
2017-07-27 21:06:42 +0800

    数据库中表notetable改名为mynotetable

--- data/show_note.php
+++ data/show_note.php
@@ -6,7 +6,7 @@
 	}
 	$count = $_POST['count'];
 	$user = $_POST['user'];
-	$sql = "SELECT id, name, title, label, content, txt, date FROM notetable WHERE name = '{$user}' ORDER BY date DESC LIMIT $start, $count";
+	$sql = "SELECT id, name, title, label, content, txt, date FROM mynotetable WHERE name = '{$user}' ORDER BY date DESC LIMIT $start, $count";
 	$result = mysqli_query($conn, $sql);
 	$output = [];
 	while (($row = mysqli_fetch_assoc($result)) !== NULL) {
