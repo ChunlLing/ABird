ee3e1079921279835cf4013aaa34ffd8f6c43f2c
ChunLing <xcl8202@qq.com>
2017-08-01 21:57:35 +0800

    插件：元素可见时触发事件

--- js/myPlugin.js
+++ js/myPlugin.js
@@ -1,4 +1,6 @@
 (function ($) {
+	var _oldShow = $.fn.show;
+	
 	$.fn.extend({
 		emailList: function (option) {
 
@@ -148,6 +150,20 @@
 			});
 
 			return this;
+		}
+
+		,show: function (speed, oldCallback) {
+			return $(this).each(function () {
+				var obj = $(this),
+					newCallback = function () {
+						if ($.isFunction(oldCallback)) {
+							oldCallback.apply(obj);
+						}
+						obj.trigger('afterShow');
+					};
+				obj.trigger('beforeShow');
+				_oldShow.apply(obj, [speed, newCallback]);
+			});
 		}
 	});
 })(jQuery);
