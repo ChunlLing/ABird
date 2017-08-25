9d966890afbdad450f48d9885b3992640314dc0c
ChunLing <xcl8202@qq.com>
2017-08-26 00:49:18 +0800

    修复重命名非第一组时从数据库获取信息错误

--- data/add_group.php
+++ data/add_group.php
@@ -9,13 +9,14 @@
 	);
 	if ($group['teamId']) {
 		$query = "UPDATE grouptable SET team = '{$group['teamName']}', description = '{$group['teamDescription']}', date = NOW() WHERE id = '{$group['teamId']}'";
+		mysqli_query($conn, $query);
+		$query = "SELECT * FROM grouptable WHERE id = '{$group['teamId']}'";
 	} else {
 		$query = "INSERT INTO grouptable (team, master, description, date) VALUES ('{$group['teamName']}', '{$group['teamMaster']}', '{$group['teamDescription']}', NOW())";
+		mysqli_query($conn, $query);
+		$query = "SELECT * FROM grouptable ORDER BY id DESC LIMIT 1";
 	}
 	
-	mysqli_query($conn, $query);
-
-	$query = "SELECT * FROM grouptable ORDER BY id DESC LIMIT 1";
 	$result = mysqli_fetch_assoc(mysqli_query($conn, $query));
 
 	echo json_encode($result);
